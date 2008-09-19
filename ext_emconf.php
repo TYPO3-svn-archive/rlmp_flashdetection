4c1edc23729f344db8d29c4eebfce0017db9f13e
commodore <commodore@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-19 08:22:50 +0000

    Fixed bug #1239 and added feature request #1298. Updated documentation. New version is now 1.3.4.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rlmp_flashdetection/trunk@12125 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "rlmp_flashdetection"
 #
-# Auto generated 27-05-2008 18:18
+# Auto generated 19-09-2008 09:49
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -11,11 +11,11 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'AST Flash (with Adobe Detection v1.5)',
+	'title' => 'AST Flash (Adobe Detection v1.6/1.5)',
 	'description' => 'This plugin provides a new record type "Flash" with advanced detection routines for most platforms and Flash versions. It features XML parameters as well as direct access from other extensions.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.3.0',
+	'version' => '1.3.4',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:21:"ext_conf_template.txt";s:4:"6204";s:12:"ext_icon.gif";s:4:"346a";s:17:"ext_localconf.php";s:4:"64fb";s:14:"ext_tables.php";s:4:"aaa2";s:14:"ext_tables.sql";s:4:"2bb3";s:24:"ext_typoscript_setup.txt";s:4:"f78a";s:41:"icon_tx_rlmpflashdetection_flashmovie.gif";s:4:"1522";s:13:"locallang.xml";s:4:"13c5";s:16:"locallang_db.xml";s:4:"b206";s:7:"tca.php";s:4:"c560";s:14:"doc/manual.sxw";s:4:"c20d";s:14:"pi1/ce_wiz.gif";s:4:"d2fd";s:39:"pi1/class.tx_rlmpflashdetection_pi1.php";s:4:"720b";s:47:"pi1/class.tx_rlmpflashdetection_pi1_wizicon.php";s:4:"6e03";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"956b";s:16:"res/AC_OETags.js";s:4:"3d6a";}',
+	'_md5_values_when_last_written' => 'a:18:{s:21:"ext_conf_template.txt";s:4:"6204";s:12:"ext_icon.gif";s:4:"346a";s:17:"ext_localconf.php";s:4:"64fb";s:14:"ext_tables.php";s:4:"aaa2";s:14:"ext_tables.sql";s:4:"4740";s:24:"ext_typoscript_setup.txt";s:4:"f78a";s:41:"icon_tx_rlmpflashdetection_flashmovie.gif";s:4:"1522";s:13:"locallang.xml";s:4:"13c5";s:16:"locallang_db.xml";s:4:"b206";s:7:"tca.php";s:4:"5885";s:14:"doc/manual.sxw";s:4:"7786";s:14:"pi1/ce_wiz.gif";s:4:"d2fd";s:39:"pi1/class.tx_rlmpflashdetection_pi1.php";s:4:"6be9";s:47:"pi1/class.tx_rlmpflashdetection_pi1_wizicon.php";s:4:"6e03";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"956b";s:16:"res/AC_OETags.js";s:4:"062f";s:19:"res/AC_OETags1.5.js";s:4:"3d6a";}',
 	'suggests' => array(
 	),
 );
